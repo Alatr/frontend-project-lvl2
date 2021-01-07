de5d3d7bf2d71679532dd4d25d346107efa28d77
Alatr <dimalukianovweb@gmail.com>
2021-01-07 19:04:52 +0200

    ignore

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,5 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander'
-const program = new Command();
-console.log('Usage: gendiff [options]');
+import gendiff from '../src/index.js';
 
-program
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
-
-program.on('-h', () => {
-  console.log('');
-  console.log('Example call:');
-  console.log('  $ custom-help --help');
-});
-
-program.parse(process.argv);
-
+gendiff();
