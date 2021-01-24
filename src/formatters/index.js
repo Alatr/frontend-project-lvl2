91bdcb20b485fd27aad6947e7826a85086efed49
Alatr <dimalukianovweb@gmail.com>
2021-01-24 13:31:04 +0200

    ci 8

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -5,13 +5,9 @@ import * as json from './ast.js';
 
 const formaters = { stylish, plain, json };
 
-export default function getFormat(formatName = undefined) {
+export default function getFormat(formatName = stylish) {
   if (!_.has(formaters, formatName)) {
     throw new Error(`Unknown format: recived format name '${formatName}'.`);
-  }
-  if (formatName === undefined) {
-    return formaters.stylish;
-    
   }
   return formaters[formatName];
 }
