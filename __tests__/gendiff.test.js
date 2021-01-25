015d134bef6e5b7bd800da016b4b0c6faa478f5d
Alatr <dimalukianovweb@gmail.com>
2021-01-25 17:12:21 +0200

    refactor_2

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.resolve(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
+/* eslint-disable fp/no-let, fp/no-mutation*/
 let resultCompareStylish;
 let resultComparePlain;
 let resultCompareAST;
@@ -35,6 +35,7 @@ beforeEach(() => {
   emptyYml = getFixturePath('empty.yml');
   emptyJSON = getFixturePath('empty.json');
 });
+/* eslint-enable fp/no-let, fp/no-mutation*/
 
 describe('gendiff main flow', () => {
   test('check main flow with json', () => {
