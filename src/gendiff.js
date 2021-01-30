db2f444c21bd525c7c8a80dd1363b29ae1fe2783
Alatr <dimalukianovweb@gmail.com>
2021-01-30 22:19:39 +0200

    fiv fin

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,10 +4,6 @@ const gendiff = (object1, object2) => {
   const keys = _.sortBy(_.union(_.keys(object1), _.keys(object2)));
 
   return keys.map((key) => {
-    if (_.isEqual(object1[key], object2[key])) {
-      return { key, value: object2[key], status: 'unchanged' };
-    }
-
     if (!_.has(object2, key)) return { key, value: object1[key], status: 'removed' };
 
     if (!_.has(object1, key)) return { key, value: object2[key], status: 'added' };
@@ -16,6 +12,10 @@ const gendiff = (object1, object2) => {
       return {
         key, value: 'complex', status: 'nested', children: gendiff(object1[key], object2[key]),
       };
+    }
+
+    if (_.isEqual(object1[key], object2[key])) {
+      return { key, value: object2[key], status: 'unchanged' };
     }
 
     return {
